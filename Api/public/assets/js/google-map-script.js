25d81b764d0c50a36d691187cbfc6c17ab9d93f4
YAPI Amand <yapiamand@gmail.com>
2022-04-25 09:06:13 +0000

    update

--- Api/public/assets/js/google-map-script.js
+++ Api/public/assets/js/google-map-script.js
@@ -1,6 +1,6 @@
 function initMap() {
     // The location of Uluru
-    var uluru = { lat: 5.350500, lng: -3.921719 };
+    var uluru = { lat: 5.362159, lng: -3.957592 };
     // The map, centered at Uluru
     var map = new google.maps.Map(
         document.getElementById('map'), {
